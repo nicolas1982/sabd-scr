4f501eb01e7d37c0d4da95aef7f6262aeba66fb9
nicolas.galarza <nicolas.galarza@15752056-113f-0410-99b6-91c273c8dec5>
2007-11-17 17:23:20 +0000

    lo vuelvo a generar al proyecto

--- layout/components/SCRPasswordField.java
+++
@@ -1,9 +0,0 @@
-package layout.components;
-
-import javax.swing.JPasswordField;
-
-public class SCRPasswordField extends JPasswordField {
-
-	private static final long serialVersionUID = 1092821636625659035L;
-
-}
